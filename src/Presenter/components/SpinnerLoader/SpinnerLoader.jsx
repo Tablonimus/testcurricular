c5aecdcad831ffb391511c0518da2eab0d924c41
tablonimus <lautamartin1995@gmail.com>
2024-08-09 06:02:18 -0300

    new commit

--- src/Presenter/components/SpinnerLoader/SpinnerLoader.jsx
+++ src/Presenter/components/SpinnerLoader/SpinnerLoader.jsx
@@ -3,7 +3,7 @@ import { Spinner } from "@material-tailwind/react";
 export default function SpinnerLoader() {
   return (
     <div className="absolute flex items-center justify-center w-screen h-screen bg-black/50 z-50">
-      <Spinner className="h-16 w-16 text-orange-800 " />
+      <Spinner className="h-16 w-16 text-[--primary] " />
     </div>
   );
 }
